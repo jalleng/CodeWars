ead6407f897fa92a31cb8ab3d6a8a541efa92121
Jeff Gerber <jallengerber@gmail.com>
2016-12-30 06:09:46 -0800

    tidy

--- kata6/yourOrderPlease.js
+++ kata6/yourOrderPlease.js
@@ -11,12 +11,7 @@ let thing = "is2 Thi1s T4est 3a";
 
 
 
-function order(words){
 
-  return words.split(' ').sort(function(a, b){
-      return a.match(/\d/) - b.match(/\d/);
-   }).join(' ');
-}
 
 function order1(words){
   let list = words.split(' ');
@@ -33,13 +28,8 @@ function order1(words){
   return (output.join(' '));
 }
 
-// var str="(12) 3456-7890";
-// str.replace( /\D+/g, '');
-
-
-
-// Test.assertEquals(order("is2 Thi1s T4est 3a"), "Thi1s is2 3a T4est")
-// Test.assertEquals(order("4of Fo1r pe6ople g3ood th5e the2"), "Fo1r the2 g3ood 4of th5e pe6ople")
-// Test.assertEquals(order(""), "")
-
-console.log (order(thing));
+function order(words){
+  return words.split(' ').sort(function(a, b){
+      return a.match(/\d/) - b.match(/\d/);
+   }).join(' ');
+}
